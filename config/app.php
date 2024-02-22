39992aa887626c6a4be050706a7c216a800d488e
facundososab <facundososabianciotto@gmail.com>
2024-02-22 14:35:26 -0300

    reacomodamiento de archivos

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php	
+    const APP_URL="http://localhost/TP_Veterinaria/";
+	const APP_NAME="Veterinaria San Anton";
+	const APP_SESSION_NAME="VET";
+
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set("America/Argentina/Buenos_Aires");
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+    require_once "./config/app.php";
+    require_once "./autoload.php";
+
+    /*---------- Iniciando sesion ----------*/
+    require_once "./app/views/inc/session_start.php";
+
+    if(isset($_GET['views'])){
+        $url=explode("/", $_GET['views']);
+    }else{
+        $url=["login"];
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
