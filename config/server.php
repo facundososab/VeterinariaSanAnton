fa6a685e34b2b8e6afe0caeee2fe81a54c7d2395
facundososab <facundososabianciotto@gmail.com>
2025-03-27 23:02:48 -0300

    actualizar configuración del servidor de base de datos para usar variables de entorno

--- config/server.php
+++ config/server.php
@@ -5,7 +5,12 @@
 // const DB_USER = "if0_37138221";
 // const DB_PASS = "HFuZ1lHpmk";
 
-// const DB_SERVER = "interchange.proxy.rlwy.net";
+const DB_SERVER = getenv("DB_SERVER");
+const DB_NAME = getenv("DB_NAME");
+const DB_USER = getenv("DB_USER");
+const DB_PASS = getenv("DB_PASS");
+const DB_PORT = getenv("DB_PORT");
+
 // const DB_NAME = "veterinaria";
 // const DB_USER = "root";
 // const DB_PASS = "ptylYEjHZmTFmGwTmGPIQmNatnUdGYNm";
